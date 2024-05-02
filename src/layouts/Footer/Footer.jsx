0fbda307ede89d0e0fd5cedfac45b063bd534121
Hitesh Prajapati <hiteshvpra@gmail.com>
2024-05-02 23:16:47 +0530

    pages created

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../Footer/Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>Footer Section</h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./../Home/Home.css";
+import Header from './../../layouts/Header/Header';
+import Footer from './../../layouts/Footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <h3>Home Page</h3>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
