539e9d8c0fa41dbf4a92d509c912f57c7daf3375
Hitesh Prajapati <hitesh.prajapati@gameium.com>
2024-05-14 17:44:47 +0530

    Remove pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
-import About_us from './pages/About_us/About_us';
-import Blog from './pages/Blog/Blog';
-import Contact_us from './pages/Contact_us/Contact_us';
-import Services from './pages/Services/Services';
 import "./Common.css"
 
 function App() {
@@ -13,9 +9,9 @@ function App() {
     <>
       <Routes>
         <Route index element={<Home />} />
-        <Route path='/about-us' element={<About_us />}/>
+        {/* <Route path='/about-us' element={<About_us />}/>
         <Route path='/services' element={<Services />}/>
-        <Route path='/contact-us' element={<Contact_us />}/>
+        <Route path='/contact-us' element={<Contact_us />}/> */}
       </Routes>
     </>
   )
